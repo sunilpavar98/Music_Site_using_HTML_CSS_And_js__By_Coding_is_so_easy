cfda1467ce1e8ab60607a87a9138bd411389e9ea
Sunil Pavar <55226366+sunilpavar98@users.noreply.github.com>
2022-11-02 18:57:38 +0530

    Music Site Using HTML CSS And Js
    Music Site Using HTML CSS And Js
    full Tutorials :- https://www.youtube.com/c/Codingissoeasy

---
+++ music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+        name: "Kina - get you the moon (slowed)",
+        artist: "Kina",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "losing interest ",
+        artist: "Shiloh Dynasty",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "tell me why i'm waiting ",
+        artist: "timmis & shiloh",
+        img: "music-3",
+        src: "music-3"
+    }
+]
---
+++ script.js
@@ -0,0 +1,44 @@
+const wrapper = document.querySelector(".wrapper"),
+    musicImg = wrapper.querySelector(".img-area img"),
+    musicName = wrapper.querySelector(".song-details .name"),
+    musicArtist = wrapper.querySelector(".song-details .artist"),
+    playPauseBtn = wrapper.querySelector(".play-pause"),
+    prevBtn = wrapper.querySelector("#prev"),
+    nextBtn = wrapper.querySelector("#next"),
+    mainAudio = wrapper.querySelector("#main-audio"),
+    progressBar = wrapper.querySelector(".progress-bar"),
+    progressArea = wrapper.querySelector(".progress-area"),
+    musicList = wrapper.querySelector(".music-list"),
+    moreMusicBtn = wrapper.querySelector("#more-music"),
+    closemoreMusic = wrapper.querySelector("#close")
+
+
+//show Music List onclick Music Icon
+
+//Load Funcion
+
+//Music List 
+
+//Playing Song
+
+//play and Pause Event
+
+//play music Function
+
+//Pause Music Function
+
+//Update Progress Bar as to music Curr Time
+
+//update Music curr time as to Progress Bar
+
+//next event
+
+//next Function
+
+//Prev Event
+
+// Prev Function
+
+//change loop shuffle and repeat icon
+
+//After End Song
